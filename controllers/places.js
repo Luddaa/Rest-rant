9751acbcfd472c827ff9add328e0feecbb77e0a6
Luddaa <dangeloeugene33@gmail.com>
2023-06-05 14:23:57 -0700

    adding bootstrap gride to index page

--- controllers/places.js
+++ controllers/places.js
@@ -1,19 +1,20 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/restaurant1.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/restaurant2.jpg'
-      }]
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/h-thai-ml-tables.png'
+  }, {
+      name: 'Coding Cat Cafe',
+      city: 'Phoenix',
+      state: 'AZ',
+      cuisines: 'Coffee, Bakery',
+      pic: '/images/coffee-cat.jpg'
+  }]
+  
       
       res.render('places/index', {places})
 })
--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,9 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <img src="public\images\sarah-kilian-52jRtc2S_VE-unsplash.jpg" alt="spilled ice cream"/>
+              photo by <a href = "author_link">Sarah killian</a> on <a href ="UNSPLASH_LINK">unsplash</a>
+
           </main>
       </Def>
     )
