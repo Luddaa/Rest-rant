3d29a69209c80defb41121414b70a78214d13fa7
Luddaa <dangeloeugene33@gmail.com>
2023-06-11 17:29:48 -0700

    added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -40,6 +40,21 @@ router.get('/:id', (req, res)=>{
   }
 })
 
+//edit
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
+
 //Delete
 router.delete('/:id', (req, res)=>{
   let id = Number(req.params.id)
