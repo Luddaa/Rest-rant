ab2a80a1ed085f694a72ee449f4e6048bb6ddc87
Luddaa <dangeloeugene33@gmail.com>
2023-06-16 11:19:42 -0700

    implemeting mongoose ODM

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,36 +2,35 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-    console.log(data, "Data info")
-  let placesFormatted = data.places.map((place, index) => {
-    return (
-        <div className="col-sm-6">
+    let placesFormatted = data.places.map((place) => {
+        return (
+          <div className="col-sm-6" key={place.id}>
             <h2>
-              <a href={`/places/${index}`}>
-                {place.name}
-                </a>
-                </h2>
+            <a href={`/places/${place.id}`}>
+            {place.name}
+            </a>
+            </h2>
             <p className="text-center">
               {place.cuisines}
             </p>
-            <img src={place.pic} alt={place.name}/>
-            <p className='text-center'>
+            <img src={place.pic} alt={place.name} />
+            <p className="text-center">
               Located in {place.city}, {place.state}
             </p>
-        </div>
-    )
-  })
+          </div>
+        )
+      })
+      
     return (
       <Def>
           <main>
-              <h1>Places to LOVE</h1>
-              <div className='row'>
-              {placesFormatted}
+              <h1>Places to Rant or Rave About</h1>
+              <div className="row">
+                {placesFormatted}
               </div>
           </main>
       </Def>
     )
-  }
-  
+  } 
 
 module.exports = index
