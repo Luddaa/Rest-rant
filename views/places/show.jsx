fcdbe5e6401edfeb1a870bfa9af19dd6985b2adc
Luddaa <dangeloeugene33@gmail.com>
2023-06-16 14:47:44 -0700

    seeding data,validations and helper method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,14 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let message =''
+    if(data.message){
+        message=(
+            <h4 className="alert-danger">
+                {data.message}
+            </h4>
+        )
+    }
     return (
         <Def>
           <main>
@@ -12,18 +20,37 @@ function show (data) {
                 </h2>
                 <p>Not Rated</p>
             </div>
-            <div>
-                <h2>
-                    Comments
-                </h2>
-                <p>No Comments Yet!</p>
+            <div className='row'>
+                <div className='col-sm-6'>
+                    <img src={data.place.pic} alt={data.place.name} />
+                    <h3>
+                        Located in {data.place.city}, {data.place.state}
+                    </h3>
+                </div>
+                <div className='col-sm-6'>
+                    <h2>
+                        Description
+                    </h2>
+                    <h3>
+                        {data.place.showEstablished()}
+                    </h3>
+                    <h4>
+                        Serving {data.place.cuisines}
+                    </h4>
+                </div>
+                <div className='col-sm-12'>
+                    <h2>
+                        Comments
+                    </h2>
+                    <p>No Comments Yet!</p>
+                </div>
             </div>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                Edit
+            <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
+            <i className="bi bi-pencil"></i> Edit
             </a>
-            <form method="POST" action={'/places/${data.id}?_method=DELETE'}>
+            <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
                 <button type="submit" className='btn btn-danger'>
-                    Delete
+                <i className="bi bi-trash"></i> Delete
                 </button>
             </form>
           </main>
